4c1d29c46f37574a42bb1b6c31300f85052f56dd
Motorwala Yusuf Husain <yusufmotorwala04@gmail.com>
2024-08-29 10:28:24 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class Loop{
+    public static void main(String[] args) {
+    //  Scanner sc=new Scanner(System.in);
+    //  int i= sc.nextInt();
+    //  int mul;
+       
+    //  for(int n=0; n<=(10*i);n=n+i){
+    //         mul=n;
+    //         System.out.println(mul);
+    //     }
+        
+     
+        // int sum=0;
+        // for(int n=0;n<=i;n++){
+        //     sum=sum+n;
+        // }
+        // System.out.println(sum);
+        
+        
+        // while(n<5){
+        //     int sum=n;
+        //     n++;
+        //     sum= sum+n;
+        //     System.out.println(sum);
+        // }
+
+
+
+
+    // int i=2;
+    // do{
+    //     System.out.println(i);
+    //     i++;
+    // }
+    // while(i<8);
+    
+    // while(i<=10){
+    //     System.out.println(i);
+    //     i++;
+    // }
+    
+    
+    for(; ;){
+        System.out.print("Hello ");
+    }
+
+}
+
+}
