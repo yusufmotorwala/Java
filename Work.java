4f57166b07f81923b7493590de1d6b1d2c24fab4
Motorwala Yusuf Husain <yusufmotorwala04@gmail.com>
2024-09-19 21:03:31 +0530

    Add files via upload

---
+++ Work.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Work{
+    public static void main(String[] args) {
+       // System.out.println("Hello");
+       Scanner sc=new Scanner(System.in);
+      int a= sc.nextInt();
+      int b = sc.nextInt();
+     if (a==b) {
+        System.out.println("Equal");
+     }
+     else if(a>b){
+        System.out.println("greater");
+     } 
+     else{
+        System.out.println("lesser");
+     }
+     
+     // int sum=(a+b);
+     //System.out.println(sum);
+      
+      /* String name=sc.nextLine();
+       System.out.println(name);*/
+       
+       /*int a =10;
+       int b =5;
+       int ans=(a+b);
+       System.out.println(ans);*/
+    
+
+
+
+    }
+}
